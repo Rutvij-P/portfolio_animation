dfaceee1882901fd39dbc6b445f4879c27aecd6d
Rutu Patel <patelrutvij2801@gmail.com>
2023-12-17 22:25:54 -0800

    Project updates

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CursorRevealText from './components/CursorRevealText';
 import Navbar from './components/Navbar';
 import Project from './components/Projects';
-import jobit from './images/jobit.png';
-import gymapp from './images/gymappss.png';
-import parallaxscroll from './images/parallaxscrollss.png';
-import vehiq from './images/vehiq.png';
-import portfoliop from './images/portfoliopagess.png';
-// ... other imports
+
+
 
 function App() {
   // Define your projects array or fetch it from an API
   const projects = [
-    { id: 1, title: "MATE", description: "Motion Graphics, Social Media, Social Learning", year: "2023", image: {jobit} },
-    { id: 2, title: "KATE", description: "Motion , Social , Social ", year: "2123", image: "../src/images/jobit.png" },
-    { id: 3, title: "FATE", description: "Motion Graphics, Social Media, Social Learning", year: "2023", image: "../src/images/parallaxscrollss.png" },  
+    { id: 1, title: "MATE", description: "Motion Graphics, Social Media, Social Learning", year: "2023", image: "/gymappss.png", bgColor: "#C793FF" },
+    { id: 2, title: "KATE", description: "Motion , Social , Social ", year: "2123", image: "/parallaxscrollss.png", bgColor: "#93FFD8" },
+    { id: 3, title: "FATE", description: "Motion Graphics, Social Media, Social Learning", year: "2023", image: "/jobit.png", bgColor: "#FFEE93" },  
     // ... other project objects
   ];
 
+  const [sectionBgColor, setSectionBgColor] = useState('');
+
+  const handleProjectHover = (color) => {
+    setSectionBgColor(color);
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: sectionBgColor}}>
       <Navbar />
       <CursorRevealText />
-      <div className="max-w-4xl mx-auto my-8 border-t-2 border-black"></div>
+      <div className="max-w mx-auto my-8 border-t-2 border-black"></div>
       {projects.map((project, index) => (
         <Project
           key={project.id}
@@ -32,6 +34,8 @@ function App() {
           year={project.year}
           image={project.image}
           last={index === projects.length - 1}
+          onHover={() => handleProjectHover(project.bgColor)}
+          onHoverEnd={() => handleProjectHover('')}
         />       
       ))}
       {/* other components */}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,11 @@
 import React, { useRef, useState } from 'react';
 
-const Project = ({ id, title, description, year, image }) => {
+const Project = ({ id, title, description, year, image, onHover, onHoverEnd }) => {
   const ref = useRef(null);
   const [showImage, setShowImage] = useState(false);
+  
   const [imageCoords, setImageCoords] = useState({ x: 0, y: 0 });
+
 
   const handleMouseMove = (event) => {
     const { clientX, clientY } = event;
@@ -19,22 +21,23 @@ const Project = ({ id, title, description, year, image }) => {
 
   const handleMouseEnter = () => {
     setShowImage(true);
+    onHover();
   };
 
   const handleMouseLeave = () => {
     setShowImage(false);
+    onHoverEnd(false);  
   };
 
-  // Debug: Log the image URL
-  console.log(`Image URL for project ${id}:`, image);
 
   return (
     <div 
       ref={ref} 
-      className="project-container flex flex-col max-w-4xl mx-auto my-8 border-b-2 border-black py-6 relative"
+      className="project-container flex flex-col max-w-6xl mx-auto my-8 border-b-2 border-black py-6 relative"
       onMouseMove={handleMouseMove}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      
     >
       {showImage && (
         <img
@@ -47,14 +50,15 @@ const Project = ({ id, title, description, year, image }) => {
             left: imageCoords.x,
             top: imageCoords.y,
             transform: 'translate(-50%, -50%)',
-            width: '200px',
+            maxWidth: '350px',
             height: 'auto',
+            maxHeight: '250px',
             pointerEvents: 'none', // Ignore the mouse events
           }}
         />
       )}
       <p className="text-2xl text-right font-light">{year}</p>
-      <div className="flex justify-between items-end mb-2">          
+      <div className="flex justify-between items-end mb-2 z-20">          
         <div className="flex gap-4">            
           <span className="text-2xl font-light">{id.toString().padStart(2, '0')}</span>
           <h3 className="text-8xl font-bold leading-none">{title}</h3>
